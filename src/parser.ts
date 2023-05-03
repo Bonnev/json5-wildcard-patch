1e358e5be38d44a25a1c76dfadc54f03c5baf689
Bonnev <piggov@gmail.com>
2023-05-03 21:41:00 +0300

    batch 8

--- src/parser.ts
+++ src/parser.ts
@@ -1,15 +1,13 @@
 import { Token, TokenType } from './tokenizer';
 
-interface Navigation {
-	navigate(input: object): string
+export enum OperationType {
+	SimpleNavigation = 'SimpleNavigation',
+	WildcardNavigation = 'WildcardNavigation',
+	END = 'END',
 }
 
-class SingleNavigation implements Navigation {
-	constructor(public next: Navigation) { }
-
-	navigate(input: object): string {
-		return 
-	}
+export class Operation {
+	constructor(public type: OperationType, public value: string | null) { }
 }
 
 class Parser {
@@ -22,15 +20,43 @@ class Parser {
 	}
 
 	parse(): Navigation {
+		for (let index = 0; index < this.tokens.length; index++) {
+			const token = this.tokens[index];
+			const nextToken = this.tokens[index + 1];
+
+			switch (token.type) {
+			case TokenType.SLASH:
+				if (nextToken.type !== TokenType.IDENTIFIER && nextToken.type !== TokenType.ANY) {
+					this.handleError(nextToken.column, 'Expected an identifier or a wildcard');
+				}
+
+				if (token.type === TokenType.IDENTIFIER) {
+					return new Operation(OperationType.SimpleNavigation, null);
+				} else {
+					return new Operation(OperationType.WildcardNavigation, null);
+				}
+			}
+		}
+
 		return this.parseRecursive(0);
 	}
 
 	parseRecursive(index: number): Navigation {
+		if (this.tokens[index].type === TokenType.END) { }
+
+		if (this.tokens[index].type == TokenType.SLASH) {
+			switch (this.tokens[index].type) {
+			case TokenType.IDENTIFIER:
+				return new SingleNavigation(this.parseRecursive(index + 2));
+			}
+		}
+
 		switch (this.tokens[index].type) {
 		case TokenType.SLASH:
 			if (this.tokens[index + 1].type !== TokenType.SLASH && this.tokens[index + 1].type !== TokenType.ANY) {
-				this.errors.push()
+				this.handleError(this.tokens[index + 1].column, 'Expected an identifier or a wildcard');
 			}
+
 			return new SingleNavigation(this.parseRecursive(index + 2));
 		default:
 			throw new Error('Invalid token type: ' + this.tokens[index].type);
@@ -42,7 +68,7 @@ class Parser {
 	}
 
 	atEnd() {
-		return this.currentIndex === this.tokens.length - 1;
+		return this.tokens[this.currentIndex].type === TokenType.END;
 	}
 }
 
