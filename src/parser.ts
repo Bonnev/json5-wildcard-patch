167a5aea064feaf3f41a7a08fadcb217dbe2c12f
Bonnev <piggov@gmail.com>
2023-05-05 18:42:23 +0300

    Initial file structure, untested logic

---
+++ src/parser.ts
@@ -0,0 +1,79 @@
+import { Token, TokenType } from './tokenizer';
+
+export enum OperationType {
+	SimpleNavigation = 'SimpleNavigation',
+	WildcardNavigation = 'WildcardNavigation',
+	END = 'END',
+}
+
+export class Operation {
+	constructor(public type: OperationType, public value: string | null) { }
+}
+
+class Parser {
+	private currentIndex: number;
+	private errors: string[] = [];
+
+	constructor(private tokens: Token[]) {
+		this.currentIndex = 0;
+		this.tokens = tokens;
+	}
+
+	parse(): Navigation {
+		const operations: Operation[] = [];
+
+		for (let index = 0; index < this.tokens.length; index++) {
+			const token = this.tokens[index];
+			const nextToken = this.tokens[index + 1];
+
+			switch (token.type) {
+			case TokenType.SLASH:
+				if (nextToken.type !== TokenType.IDENTIFIER && nextToken.type !== TokenType.ANY) {
+					this.handleError(nextToken.column, 'Expected an identifier or a wildcard');
+				}
+
+				if (nextToken.type === TokenType.IDENTIFIER) {
+					operations.push(new Operation(OperationType.SimpleNavigation, token.value));
+				} else {
+					operations.push(new Operation(OperationType.WildcardNavigation, null));
+				}
+			}
+		}
+
+		return this.parseRecursive(0);
+	}
+
+	parseRecursive(index: number): Navigation {
+		if (this.tokens[index].type === TokenType.END) { }
+
+		if (this.tokens[index].type == TokenType.SLASH) {
+			switch (this.tokens[index].type) {
+			case TokenType.IDENTIFIER:
+				return new SingleNavigation(this.parseRecursive(index + 2));
+			}
+		}
+
+		switch (this.tokens[index].type) {
+		case TokenType.SLASH:
+			if (this.tokens[index + 1].type !== TokenType.SLASH && this.tokens[index + 1].type !== TokenType.ANY) {
+				this.handleError(this.tokens[index + 1].column, 'Expected an identifier or a wildcard');
+			}
+
+			return new SingleNavigation(this.parseRecursive(index + 2));
+		default:
+			throw new Error('Invalid token type: ' + this.tokens[index].type);
+		}
+	}
+
+	handleError(column: number, message: string) {
+		this.errors.push(`at column ${column}: ${message}`);
+	}
+
+	atEnd() {
+		return this.tokens[this.currentIndex].type === TokenType.END;
+	}
+}
+
+export default function parse(tokens: Token[]): Navigation {
+	return new Parser(tokens).parse();
+}
---
+++ src/tokenizer.ts
@@ -0,0 +1,83 @@
+export enum TokenType {
+	SLASH = 'SLASH',
+	IDENTIFIER = 'IDENTIFIER',
+	ANY = 'ANY',
+	END = 'END',
+}
+
+export class Token {
+	constructor(public column: number, public type: TokenType, public value: string | null) { }
+}
+
+class Tokenizer {
+	private currentIndex: number;
+	private errors: string[] = [];
+
+	constructor(private text: string) {
+		this.currentIndex = 0;
+	}
+
+	tokenize(): Token[] {
+		const tokens: Token[] = [];
+		this.currentIndex = 0;
+
+		if (this.text[this.currentIndex] !== '/') {
+			this.handleError('Expected \'/\' at start');
+			return [];
+		}
+		tokens.push(new Token(this.currentIndex, TokenType.SLASH, null));
+		this.currentIndex++;
+
+		while(!this.atEnd()) {
+			const character = this.text[this.currentIndex];
+
+			switch(character) {
+			case '/':
+				tokens.push(new Token(this.currentIndex, TokenType.SLASH, null));
+				break;
+			case '*':
+				tokens.push(new Token(this.currentIndex, TokenType.ANY, null));
+				break;
+			default:
+				if (this.isIdentifier(character)) {
+					const id = this.consumeIdentifier();
+					tokens.push(new Token(this.currentIndex, TokenType.IDENTIFIER, id));
+				}
+				break;
+			}
+
+			this.currentIndex++;
+		}
+		tokens.push(new Token(this.currentIndex, TokenType.END, null));
+
+		return tokens;
+	}
+
+	isIdentifier(character: string): boolean {
+		return /[a-zA-Z0-9_]/g.test(character);
+	}
+
+	consumeIdentifier() {
+		let id = '';
+
+		while (!this.atEnd() && this.isIdentifier(this.text[this.currentIndex])) {
+			id += this.text[this.currentIndex];
+			this.currentIndex++;
+		}
+		this.currentIndex--;
+
+		return id;
+	}
+
+	handleError(message: string) {
+		this.errors.push(`at column ${this.currentIndex}: ${message}`);
+	}
+
+	atEnd() {
+		return this.currentIndex === this.text.length - 1;
+	}
+}
+
+export default function tokenize(text: string): Token[] {
+	return new Tokenizer(text).tokenize();
+}
